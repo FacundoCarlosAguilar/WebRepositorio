025bfbe0835b96633c8825a45fc80ed143f58116
FacundoAguilar <faguilar@alumnos.unsada.edu.ar>
2025-10-04 02:57:15 -0300

    Commit Array

--- ArrayActividadN1.java
+++ ArrayActividadN1.java
@@ -8,7 +8,7 @@ public class ArrayActividadN1 {
     public static void main(String[] args) {
         List<Integer> lista = new ArrayList<>();
         Random ramdom = new Random();
-
+|
         for (int i = 1; i < 10; i++){
             lista.add(ramdom.nextInt(1000) + 1); 
         }    
@@ -40,6 +40,8 @@ public class ArrayActividadN1 {
         
         System.out.println("El número más alto: " + max + " en la posición: " + posicionMax);
         System.out.println("El número más bajo: " + min + " en la posición: " + posicionMin);
+        System.out.println("");
+        
 
     }
 }
